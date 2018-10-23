93478d1292252a1b82b70e168f5f6bca298e88d9
annassdan <annassdan@gmail.com>
2018-10-23 14:55:17 +0700

    Initial upgrading

---
+++ src/main/java/e/brpl/eBRPL/System/EBrplSystemApplication.java
@@ -0,0 +1,12 @@
+package e.brpl.eBRPL.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBrplSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBrplSystemApplication.class, args);
+	}
+}
