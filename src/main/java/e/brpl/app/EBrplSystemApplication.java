41586b80fc12f1ce6636a92ecbc971521661499d
annassdan <annassdan@gmail.com>
2018-10-24 21:53:59 +0700

    add zone setting

--- src/main/java/e/brpl/app/EBrplSystemApplication.java
+++ src/main/java/e/brpl/app/EBrplSystemApplication.java
@@ -1,12 +1,21 @@
-package e.brpl;
+package e.brpl.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
+@SuppressWarnings("unused")
 public class EBrplSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EBrplSystemApplication.class, args);
+	}
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 }
