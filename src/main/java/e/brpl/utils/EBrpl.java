cc4456cd1a62329057f47fd69edd8939570bfd56
annassdan <annassdan@gmail.com>
2018-10-24 11:44:01 +0700

    adding new parameter

--- src/main/java/e/brpl/utils/EBrpl.java
+++ src/main/java/e/brpl/utils/EBrpl.java
@@ -9,6 +9,8 @@ public interface EBrpl {
     String TIME_PATTERN = "HH:mm";
     String DATE_TIME_PATTERN = "dd/MM/yyyy HH:mm:ss.SSS";
     String PK_PATH_VARIABLE = "pk";
+    String DIVIDER = "_";
+    String SLASH = "/";
 
     interface PAGING {
         String PAGE = "page";
@@ -20,19 +22,38 @@ public interface EBrpl {
     }
 
     interface COMPONENT {
+        String MASTER_COMPONENT = "master";
         interface MASTER {
             String DAERAH_PENANGKAPAN = "daerahpenangkapan";
             String SUMBER_DAYA = "sumberdaya";
             String SPESIES = "spesies";
         }
+
+        String ADMINISTRATOR_COMPONENT = "administrator";
+        interface ADMINISTRATOR {
+
+        }
+
+        String CORE_COMPONENT = "ebrpl";
         interface CORE {
 
+        }
+
+        String INTEGRATION_COMPONENT = "eksternal";
+        interface INTEGRATION {
+            String BOAT = "boat";
+            String BOAT_TRACKER = "boat_tracker";
+            String DEEPSLOPE = "deepslope";
+            String FINDMESPOT = "findmespot";
+            String SIZING = "sizing";
+            String TRACKER = "tracker";
         }
     }
 
     interface PATH_PREFIX {
         String MASTER = "/api/master/";
         String CORE = "/api/core/";
+        String INTEGRATION = "/api/integration/";
     }
 
 
--- src/main/java/e/brpl/utils/rest/EBrplRest.java
+++ src/main/java/e/brpl/utils/rest/EBrplRest.java
@@ -4,6 +4,7 @@ import e.brpl.utils.EBrpl;
 import e.brpl.utils.entity.EBrplEntityEvent;
 import e.brpl.utils.service.EBrplServiceEvent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 import javax.validation.Valid;
-import java.util.List;
+
 
 /**
  *
@@ -24,11 +25,13 @@ public class EBrplRest<Entity extends EBrplEntityEvent, Service extends EBrplSer
     implements EBrplRestEvent<Entity> {
 
     @Autowired
+    @SuppressWarnings("")
     protected Service service;
 
 
     @Override
     @PostMapping
+    @SuppressWarnings("unchecked")
     public ResponseEntity<Entity> save(@Valid Entity entity) {
         entity = (Entity) service.save(entity);
         return new ResponseEntity<>(entity, HttpStatus.OK);
@@ -37,6 +40,7 @@ public class EBrplRest<Entity extends EBrplEntityEvent, Service extends EBrplSer
 
     @Override
     @PutMapping("{" + EBrpl.PK_PATH_VARIABLE + "}")
+    @SuppressWarnings("unchecked")
     public ResponseEntity<Entity> edit(String uuid, @Valid Entity entity) {
 
         boolean finded = service.findOne(uuid) != null;
@@ -50,6 +54,7 @@ public class EBrplRest<Entity extends EBrplEntityEvent, Service extends EBrplSer
 
     @Override
     @GetMapping("{" + EBrpl.PK_PATH_VARIABLE + "}")
+    @SuppressWarnings("unchecked")
     public ResponseEntity<Entity> findOne(String uuid) {
         Entity entity = (Entity) service.findOne(uuid);
 
@@ -58,6 +63,7 @@ public class EBrplRest<Entity extends EBrplEntityEvent, Service extends EBrplSer
 
     @Override
     @DeleteMapping("{" + EBrpl.PK_PATH_VARIABLE + "}")
+    @SuppressWarnings("unchecked")
     public ResponseEntity<Entity> delete(String uuid) {
         Entity entity = (Entity) service.findOne(uuid);
         boolean deleted = service.delete(uuid);
@@ -66,7 +72,9 @@ public class EBrplRest<Entity extends EBrplEntityEvent, Service extends EBrplSer
 
     @Override
     @GetMapping(params = {PAGING.PAGE, PAGING.SIZE} )
+    @SuppressWarnings("unchecked")
     public ResponseEntity<?> findAll(int page, int size) {
-        return null;
+        Page<Entity> entities = service.findAll(page, size);
+        return new ResponseEntity<>(entities, HttpStatus.OK);
     }
 }
